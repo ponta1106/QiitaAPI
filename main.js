13558f540acbcce21b98f701c6e3874bb8380528
Tabata Nobuya <59525219+pontasanta@users.noreply.github.com>
2021-04-23 12:39:17 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,51 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    items: null,
+    keyword: '',
+    message: ''
+  },
+  watch: {
+    keyword: function(newKeyword, oldKeyward) {
+      // console.log(newKeyword, oldKeyward)
+      this.message = 'Waiting for you stop typing...'
+      this.debouncedGetAnswer()
+    }
+
+  },
+  created: function() {
+    // this.keyword = 'Javascript'
+    // this.getAnswer()
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.keyword === '') {
+        this.items = null
+        this.message = ''
+        return
+      }
+
+      this.message = 'Loading...'
+      var vm = this
+      var params = {
+        page: 1,
+        per_page: 100,
+        query: this.keyword
+      }
+
+      axios.get('https://qiita.com/api/v2/items', { params })
+        .then(function(response) {
+          console.log(response)
+          vm.items = response.data
+        })
+        .catch(function(error) {
+          vm.message = 'Error!' + error
+        })
+        .finally(function() {
+          vm.message = ''
+        })
+    }
+  }
+
+})
